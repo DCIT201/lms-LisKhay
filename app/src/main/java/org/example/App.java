a6ad3f3ba9b5df899ba2f9278ef7e8c22276d66e
LisKhay <kobbykhay8899@gmail.com>
2024-12-12 00:39:34 +0000

    update to assignment

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,14 +1,32 @@
-/*
- * This should be your main class where all your objects will be created
- */
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Library library = new Library();
+
+        // Add books to the library
+        library.addBook(new Book("The Girl Who can", "Ama Ataa Aidoo", 1996));
+        library.addBook(new Book("The Generous Hunter", "AA Amoako", 1999));
+
+        // List all books
+        System.out.println("Books in the library:");
+        library.listBooks();
+
+        // Create a patron
+        Patron patron = new Patron("Atakora", 1);
+
+        // Borrow a book
+        patron.borrowBook(new Book("The Generous Hunter", "AA Amoako", 1999), library);
+
+        // List borrowed books
+        System.out.println("\nBorrowed books:");
+        patron.listBorrowedBooks();
+
+        // Return a book
+        patron.returnBook(new Book("The Generous Hunter", "AA Amoako", 1999), library);
+
+        // Final library state
+        System.out.println("\nBooks in the library after operations:");
+        library.listBooks();
     }
 }
